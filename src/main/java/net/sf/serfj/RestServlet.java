76c3c5f9325e9c5d83e774af39215b9b93b08c66
Eduardo Yáñez Parareda <eduardo.yanez@gmail.com>
2010-07-27 15:42:31 +0000

    Added text for Apache 2 license

--- src/main/java/net/sf/serfj/RestServlet.java
+++ src/main/java/net/sf/serfj/RestServlet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Eduardo Yáñez Parareda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.serfj;
 
 import java.io.IOException;
@@ -72,6 +87,16 @@ public class RestServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// Eliminamos de la URI el contexto
+	    // TODO Cambiar la llamada por esto, probarlo...
+	    /*
+	     * String path = req.getPathInfo();
+               if (path == null) {
+                       path = req.getServletPath();
+                       if (path == null) {
+                               throw new ServletException(UNDEFINED_PATH);
+                       }
+               }
+	     */
 		String url = request.getRequestURI().substring(request.getContextPath().length());
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug("url => {}", url);
--- src/main/java/net/sf/serfj/annotations/DoNotRenderPage.java
+++ src/main/java/net/sf/serfj/annotations/DoNotRenderPage.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Eduardo Yáñez Parareda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.serfj.annotations;
 
 import java.lang.annotation.Retention;
@@ -5,14 +20,14 @@ import java.lang.annotation.RetentionPolicy;
 
 /**
  * With this runtime annotation, a developer is able to mark a controller's
- * method in order to not render a page after its execution.
+ * method in order to not render a page after its execution.<br><br>
  * 
  * It's only valid to methods that return void, because methods that return an
- * object, will always serialize that object in the response.
+ * object, will always serialize that object in the response.<br><br>
  * 
  * Methods that don't return anything have to be annotated so the framework is
  * able to know what to do after the execution. If it's annotated with
- * NotRenderPage, the framework will return a HTTP status code 204 (No content),
+ * DoNotRenderPage, the framework will return a HTTP status code 204 (No content),
  * if it isn't annotated, the framework will search for a page to render.
  * 
  * @author eduardo.yanez
