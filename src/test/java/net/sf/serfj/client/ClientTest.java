36864f66391694b8622b844d1ffd20af95b35468
Eduardo Yáñez Parareda <eduardo.yanez@gmail.com>
2012-03-14 11:55:31 +0000

    * test for not reached server

--- src/test/java/net/sf/serfj/client/ClientTest.java
+++ src/test/java/net/sf/serfj/client/ClientTest.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2010 Eduardo Yáñez Parareda
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,33 +12,43 @@
  */
 package net.sf.serfj.client;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import net.sf.serfj.client.Client;
+import java.io.IOException;
+import java.net.ConnectException;
 
 import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Tests for {@link Client} class.
- * 
  */
 public class ClientTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	/**
-	 * Test method for
-	 * {@link net.sf.serfj.client.Client#postRequest(java.lang.String, java.util.Map)}
-	 * .
-	 */
-	@Test
-	public void testPostRequest() {
-	}
+    /**
+     * Test method for {@link net.sf.serfj.client.Client#postRequest(java.lang.String, java.util.Map)} .
+     */
+    @Test
+    public void testPostRequest() {
+    }
 
+    @Test
+    public void testServerDown() throws IOException, WebServiceException {
+        try {
+            Client client = new Client("http://localhost:1234");
+            client.getRequest("", null);
+            fail("Server doesn't exist, you can't reach this line");
+        } catch (ConnectException e) {
+            // Ok
+            assertTrue(true);
+        }
+    }
 }
